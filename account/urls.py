9bc2ccc19aa10f994c805d52757baffcd77660e1
Igor <Aleshichev.git@email.ua>
2024-03-30 16:20:22 +0200

    added login logout

--- account/urls.py
+++ account/urls.py
@@ -10,4 +10,12 @@ urlpatterns = [
         lambda request:render(request, 'account/email/email-verification-sent.html'), 
         name='email-verification-sent'
         ),
+    #Login and Logout
+    path('login/', views.login_user, name="login"),
+    path('logout/', views.logout_user, name="logout"),
+    
+    #Dashboard
+    path('dashboard/', views.dashboard_user, name="dashboard"),
+    path('profile-management/', views.profile_user, name="profile-management"),
+    path('delete-user/', views.delete_user, name="delete-user"),
     ]
