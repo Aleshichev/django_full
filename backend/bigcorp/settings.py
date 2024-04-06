5d390692b1ca7bdef0911843081a3a778d4950a1
Igor <Aleshichev.git@email.ua>
2024-04-06 16:17:03 +0300

    added many changes

--- backend/bigcorp/settings.py
+++ backend/bigcorp/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import environ
-
+from django.contrib import messages
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 ENV_DIR = Path(__file__).resolve().parent.parent.parent
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "shop.apps.ShopConfig",
     "cart.apps.CartConfig",
     "payment.apps.PaymentConfig",
+    "recommend.apps.RecommendConfig",
 ]
 
 MIDDLEWARE = [
@@ -131,6 +132,14 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
--- backend/bigcorp/urls.py
+++ backend/bigcorp/urls.py
@@ -10,8 +10,9 @@ urlpatterns = [
     path('shop/', include('shop.urls', namespace='shop')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('account/', include('account.urls', namespace='account')),
-    path('email/', include(email_urls), name='email-verification'),
     path('payment/', include('payment.urls', namespace='payment')),
+    path("recommend/", include('recommend.urls', namespace='recommend')),
+    path('email/', include(email_urls), name='email-verification'),
     path('', views.index, name='index'),
 
 ]
